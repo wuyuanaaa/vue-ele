6eadd7022e9f07c1d767cd328e6b40f646b0e866
yuanaaa <dao33508553@163.com>
2018-09-26 23:12:19 +0800

    '商品评价部分'

--- src/components/food/food.vue
+++ src/components/food/food.vue
@@ -38,7 +38,35 @@
           <split v-show="food.info"></split>
           <div class="rating">
             <h3 class="title">商品评价</h3>
-            <ratingselect></ratingselect>
+            <ratingselect
+              ref="ratingselect"
+              :desc="desc"
+              :ratings="food.ratings"
+            ></ratingselect>
+            <div class="rating-wrapper">
+              <ul v-show="food.ratings && food.ratings.length">
+                <li
+                  v-show="needShow(rating.rateType, rating.text)"
+                  v-for="(rating, index) in food.ratings"
+                  :key="index"
+                  class="rating-item border-1px"
+                >
+                  <div class="user">
+                    <span class="name">{{rating.username}}</span>
+                    <img class="avatar" width="12" height="12" :src="rating.avatar">
+                  </div>
+                  <div class="time">{{rating.rateTime}}</div>
+                  <p class="text">
+                    <i
+                      class="thumb"
+                      :class="{'icon-thumb_up': rating.rateType === 0, 'icon-thumb_down': rating.rateType === 1}"
+                    ></i>
+                    <span class="rating-text">{{rating.text}}</span>
+                  </p>
+                </li>
+              </ul>
+              <div class="no-rating" v-show="!food.ratings || !food.ratings.length">暂无评价</div>
+            </div>
           </div>
         </div>
       </div>
@@ -61,13 +89,19 @@
     },
     data () {
       return {
-        showFlag: true
+        showFlag: false,
+        desc: {
+          all: '全部',
+          positive: '推荐',
+          negative: '吐槽'
+        }
       };
     },
     methods: {
       show () {
         this.showFlag = true;
         this.$nextTick(() => {
+          this.$refs.ratingselect.reload();
           if (!this.scroll) {
             this.scroll = new BScroll(this.$refs.food, {
               click: true
@@ -90,6 +124,9 @@
       },
       cartAdd () {
         this.$emit('cart-add', event.target);
+      },
+      needShow (type, text) {
+        return true;
       }
     },
     components: {
@@ -101,6 +138,7 @@
 </script>
 
 <style lang="stylus" rel="stylesheet/stylus">
+  @import "../../common/stylus/mixin.styl"
   .food
     position fixed
     top: 0
@@ -187,7 +225,7 @@
           transition all 0.2s
         .fade-enter, .fade-leave-to
           opacity 0
-      .desc, .rating
+      .desc
         padding: 18px
         .title
           margin-bottom: 6px
@@ -198,6 +236,52 @@
           font-size: 12px
           line-height: 24px
           color: rgb(77,85,93)
+      .rating
+        padding-top: 18px
+        .title
+          padding 0 18px
+          margin-bottom: 6px
+          font-size: 14px
+          color: rgb(7,17,27)
+        .rating-wrapper
+          padding 0 18px
+          .rating-item
+            position relative
+            padding 16px 0
+            border-bottom-1px(rgba(7,17,27,0.1))
+            .user
+              position absolute
+              right: 0
+              top: 16px
+              font-size: 0
+              .name
+                margin-right: 6px
+                line-height: 12px
+                font-size: 10px
+                color: rgb(147,153,159)
+              .avatar
+                display inline-block
+                vertical-align top
+                border-radius 50%
+            .time
+              margin-bottom: 6px
+              font-size: 10px
+              line-height: 12px
+              color: rgb(147,153,159)
+            .text
+              font-size: 0
+              .thumb
+                margin-right: 4px
+                font-size: 12px
+                line-height: 16px
+                &.icon-thumb_up
+                  color: rgb(0,160,220)
+                &.icon-thumb_down
+                  color: rgb(147,153,159)
+              .rating-text
+                line-height: 16px
+                font-size: 12px
+                color: rgb(7,17,27)
   .move-enter, .move-leave-to
     transform translate3d(100%,0,0)
 </style>
--- src/components/ratingselect/ratingselect.vue
+++ src/components/ratingselect/ratingselect.vue
@@ -1,11 +1,23 @@
 <template>
   <div class="ratingselect">
-    <div class="rating-type">
-      <span>{{desc.all}}</span>
-      <span>{{desc.positive}}</span>
-      <span>{{desc.negative}}</span>
+    <div class="rating-type border-1px">
+      <span
+        class="block positive"
+        :class="{'active': selectType === 2}"
+        @click="select(2)"
+      >{{desc.all}}<span class="count">{{ratings.length}}</span></span>
+      <span
+        class="block positive"
+        :class="{'active': selectType === 0}"
+        @click="select(0)"
+      >{{desc.positive}}<span class="count">{{positives.length}}</span></span>
+      <span
+        class="block negative"
+        :class="{'active': selectType === 1}"
+        @click="select(1)"
+      >{{desc.negative}}<span class="count">{{negatives.length}}</span></span>
     </div>
-    <div class="switch">
+    <div @click="toggleContent" class="switch border-1px" :class="{'on': onlyContent}">
       <span class="icon-check_circle"></span>
       <span class="text">只看有内容的评价</span>
     </div>
@@ -13,9 +25,10 @@
 </template>
 
 <script>
-  // const POSITIVE = 0;
-  // const NEGATIVE = 4;
   const ALL = 2;
+  const POSITIVE = 0;
+  const NEGATIVE = 1;
+  const DEFAULT_ONLY_CONTENT = true;
   export default {
     name: 'ratingselect',
     props: {
@@ -24,14 +37,6 @@
         default () {
           return [];
         }
-      },
-      selectType: {
-        type: Number,
-        default: ALL
-      },
-      onlyContent: {
-        type: Boolean,
-        default: false
       },
       desc: {
         type: Object,
@@ -43,10 +48,84 @@
           };
         }
       }
+    },
+    data () {
+      return {
+        selectType: ALL,
+        onlyContent: DEFAULT_ONLY_CONTENT
+      };
+    },
+    computed: {
+      positives () {
+        return this.ratings.filter((rating) => {
+          return rating.rateType === POSITIVE;
+        });
+      },
+      negatives () {
+        return this.ratings.filter((rating) => {
+          return rating.rateType === NEGATIVE;
+        });
+      }
+    },
+    methods: {
+      reload () {
+        this.selectType = ALL;
+        this.onlyContent = DEFAULT_ONLY_CONTENT;
+      },
+      select (type) {
+        this.selectType = type;
+      },
+      toggleContent () {
+        this.onlyContent = !this.onlyContent;
+      }
     }
   };
 </script>
 
 <style lang="stylus" rel="stylesheet/stylus">
-
+  @import "../../common/stylus/mixin.styl"
+  .ratingselect
+    .rating-type
+      padding 18px 0
+      margin 0 18px
+      border-bottom-1px(rgba(7,17,27,0.1))
+      font-size: 0
+      .block
+        display inline-block
+        padding: 8px 12px
+        margin-right: 8px
+        line-height: 16px
+        border-radius 1px
+        font-size: 12px
+        color: rgb(77,85,93)
+        &.active
+          color: #fff
+        .count
+          margin-left: 2px
+          font-size: 8px
+        &.positive
+          background: rgba(0,160,220,0.2)
+          &.active
+            background: rgba(0,160,220,1)
+        &.negative
+          background: rgba(77,85,93,0.2)
+          &.active
+            background: rgba(77,85,93,1)
+  .switch
+    padding 12px 18px
+    line-height: 24px
+    border-bottom-1px(rgba(7,17,27,0.1))
+    font-size: 0
+    color: rgb(147,153,159)
+    &.on
+      .icon-check_circle
+        color: #00c850
+    .icon-check_circle
+      display inline-block
+      vertical-align: top
+      margin-right: 4px
+      font-size: 24px
+    .text
+      display inline-block
+      font-size: 12px
 </style>
